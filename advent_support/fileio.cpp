b85d7162e698afc5a42fc2d2cc5af752ac9bb0db
Damian Barabonkov <damianb@mit.edu>
2023-12-03 00:06:55 +0100

    Created advent support directory for common utilities

---
+++ advent_support/fileio.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> readFileAsLines(const std::string& filename) {
+  std::ifstream file{ filename };
+
+  // Check if the file was opened correctly
+  if (!file.is_open()) {
+    throw std::runtime_error("Error opening file: " + filename);
+  }
+
+  std::vector<std::string> lines;
+  
+  // Read each line from the string
+  std::string line;
+  while (std::getline(file, line)) {
+    lines.push_back(line);
+  }
+
+  // Clean up
+  file.close();
+
+  // Move semantics makes this an OK thing to do
+  return lines;
+}
--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cstdint>
-#include <fstream>
 #include <vector>
 #include <string>
 #include <string_view>
@@ -9,28 +8,7 @@
 #include <map>
 #include <algorithm>
 
-std::vector<std::string> readFileAsLines(const std::string& filename) {
-  std::ifstream file{ filename };
-
-  // Check if the file was opened correctly
-  if (!file.is_open()) {
-    throw std::runtime_error("Error opening file: " + filename);
-  }
-
-  std::vector<std::string> lines;
-  
-  // Read each line from the string
-  std::string line;
-  while (std::getline(file, line)) {
-    lines.push_back(line);
-  }
-
-  // Clean up
-  file.close();
-
-  // Move semantics makes this an OK thing to do
-  return lines;
-}
+#include "advent_support/fileio.h"
 
 template <typename T>
 char firstNumber(T it, const T end) {
