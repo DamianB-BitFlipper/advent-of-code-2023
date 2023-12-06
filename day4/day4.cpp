6d5a53261451db49ab33d7de0199334b4d1cc080
Damian Barabonkov <damianb@alum.mit.edu>
2023-12-06 13:45:03 +0100

    Solved day 4

--- day4/day4.cpp
+++ day4/day4.cpp
@@ -1,10 +1,14 @@
+#include <algorithm>
 #include <cassert>
 #include <cstdint>
 #include <iostream>
+#include <memory>
+#include <numeric>
 #include <regex>
 #include <set>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 #include "advent_support/fileio.h"
 
@@ -16,8 +20,8 @@ void parseCard(const std::string &cardLine,
 
   std::smatch matches;
   const std::regex cardHeaderRegex{"Card\\s+\\d+:"};
-  const std::regex numberRegex{"\\s{1,2}(\\d+)"};
-  const std::regex dividerRegex{" \\| "};
+  const std::regex numberRegex{"^\\s{1,2}(\\d+)"};
+  const std::regex dividerRegex{"^ \\|"};
 
   // Consume the card header
   if (std::regex_search(substring_cbegin, substring_cend, matches,
@@ -98,16 +102,65 @@ void partA(const std::string &filename) {
   return;
 }
 
+void partB(const std::string &filename) {
+  std::vector<std::string> lines{readFileAsLines(filename)};
+  std::unique_ptr<int32_t[]> nCardCopies{new int32_t[lines.size()]};
+
+  // Initialize the number of copies of each card to 1
+  std::fill(nCardCopies.get(), nCardCopies.get() + lines.size(), 1);
+
+  int32_t cardId{0};
+  for (const std::string &line : lines) {
+    std::set<int32_t> winningNumbers;
+    std::set<int32_t> trialNumbers;
+
+    // Parse the card
+    parseCard(line, winningNumbers, trialNumbers);
+
+    // Find the intersection of `winningNumbers` and `trialNumbers`
+    std::set<int32_t> intersection;
+    std::set_intersection(winningNumbers.cbegin(), winningNumbers.cend(),
+                          trialNumbers.cbegin(), trialNumbers.cend(),
+                          std::inserter(intersection, intersection.begin()));
+
+    const int32_t nCopies{nCardCopies[cardId]};
+    const int32_t nIntersection{static_cast<int32_t>(intersection.size())};
+
+    // Add `nCopies` to `nCardCopies` for each card
+    // from [cardId + 1, cardId + nIntersection]
+    for (int32_t i{cardId + 1}; i <= cardId + nIntersection; ++i) {
+      // Prevent an overflow
+      if (cardId + nIntersection >= static_cast<int32_t>(lines.size())) {
+        break;
+      }
+
+      nCardCopies[i] += nCopies;
+    }
+
+    // Increment the card ID
+    cardId++;
+  }
+
+  const int32_t cumulativeCopies{
+      std::accumulate(nCardCopies.get(), nCardCopies.get() + lines.size(), 0)};
+
+  std::cout << "Part B: The cumulative number of copies is: "
+            << cumulativeCopies << std::endl;
+
+  return;
+}
+
 int32_t main(int argc, char *argv[]) {
   // Check that the filename is provided
   // if (argc != 2) {
   //   std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
   //   return 1;
   // }
-  const std::string filename{argc == 2 ? argv[1] : "input.txt"};
+  const std::string filename{argc == 2 ? argv[1] : "input_small.txt"};
 
   try {
     partA(filename);
+    partB(filename);
   } catch (const std::exception &exception) {
     std::cerr << exception.what() << std::endl;
     return 1;
